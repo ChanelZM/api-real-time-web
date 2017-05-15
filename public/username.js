ad3fc6dc98f71ff44c39cf75e88dc7f61e6aa086
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-05-15 14:36:25 +0200

    Fixed #4

--- public/username.js
+++ public/username.js
@@ -2,9 +2,11 @@
 (function(){
     var socket = io();
 
+    //Select inputs
     var userForm = document.querySelector('.username-form');
     var usernameInput = document.querySelector('#username');
 
+    //On submit send username to server.
     userForm.addEventListener('submit', function() {
         socket.emit('user', usernameInput.value);
     });
